a0c4c0735f90ea872797cc746d5f6eeb879160ff
unknown <garthbhammonds@gmail.com>
2021-02-02 17:49:15 -0800

    added to README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# notetaker
-Application that can be used to write, save, and delete notes
+# Note Taker
+Application that can be used to write, save, and delete your notes
+
+**Technologies used: Javascript, Node.js, Package.json (Express and UUID) **
+
+-------------------------------
+
+I spent quite a few hours on this assignment. Routing was pretty easy, mostly copy and paste from previous assignments but using a json file as a database had it's challenges. I originally was adding the info but when I restart the server, it would all disappear. I figured out the readFile and writeFile but I feel it was a bit convoluted.
+
+Overall I felt this assignment was challenging, just for the reason of having to figure out the read/writeFile and translating the data of the .json file.
+
+-------------------------------
+
+## Screenshot of Note page : <img src="public/assets/images/Capture-1.jpg" alt="Notes"/>
+
+## Screenshot of added note (I promise I didnt just manipulate the .json file) : <img src="public/assets/images/Capture-2.jpg" alt=""/>
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,47 +2,44 @@ const router = require('express').Router();
 const fs = require('fs');
 const uuid = require('uuid');
 
-// ------
-
+// Posts info from the database file to the HTML
 router.get('/notes', (req, res) => {
-    // const notes = fs.readFile('/db.db.json');
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        let file = JSON.parse(data);
-        res.json(file)
+        let dbFile = JSON.parse(data);
+        res.json(dbFile)
     });
 })
 
-// ------
-
+// Posts changes from the HTML to the database file
 router.post('/notes', (req, res) => {
     let newNote = req.body;
     newNote.id = uuid.v4();
     console.log("endpoint hit");
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        let file = JSON.parse(data);
-        file.push(newNote);
-        fs.writeFile('./db/db.json', JSON.stringify(file), (err) => {
+        let dbFile = JSON.parse(data);
+        dbFile.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(dbFile), (err) => {
             if (err) {console.log("oops...it broke")}
         })
     })
     res.send()
 });
 
-// ------
-
+// Function to delete a previously submitted note
+// Reads the database file, parses the data, for loop deletes note with corresponding id
 router.delete('/notes/:id', (req, res) => {
     const currentID = req.params.id;
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        let file = JSON.parse(data);
-        for (let i = 0; i < file.length; i++) {
-            if (currentID === file[i].id) {
-                file.splice(i, 1)
+        let dbFile = JSON.parse(data);
+        for (let i = 0; i < dbFile.length; i++) {
+            if (currentID === dbFile[i].id) {
+                dbFile.splice(i, 1)
             }
         }
-        fs.writeFile('./db/db.json', JSON.stringify(file), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(dbFile), (err) => {
             if (err) {console.log("whoops...it broke")}
         })
     })
